cdkey/systemtap

commit de253786000cc3960f19d9852863a8ab5a63f438

Tapset: proc_mem_rss() now includes resident shared mem pages.
The return value finally matches the VmRSS field in /proc/PID/status
now.

The old behavior can be restored by the --compatible=4.2 option
on the command line.

Thanks Dejiang Zhu for reporting this issue.

tapset/linux/proc_mem.stp
@@ -71,6 +71,13 @@ enum {
 	return @const("MM_ANONPAGES")
 }
 
+%(systemtap_v > "4.2" %?
+@__private30 function _MM_SHMEMPAGES:long()
+{
+	return @const("MM_SHMEMPAGES")
+}
+%)
+
 @__private30 function _stp_get_mm_counter:long(mm_ptr:long, member:long)
 {
     mm = & @mm(mm_ptr)
@@ -167,7 +174,11 @@ function proc_mem_rss:long ()
     mm = @task(task)->mm
     if (mm != 0)
       return (_stp_get_mm_counter(mm, _MM_FILEPAGES())
-	      + _stp_get_mm_counter(mm, _MM_ANONPAGES()))
+	      + _stp_get_mm_counter(mm, _MM_ANONPAGES())
+%(systemtap_v > "4.2" %?
+	      + _stp_get_mm_counter(mm, _MM_SHMEMPAGES())
+%)
+              )
   }
   return 0
 }
@@ -188,7 +199,11 @@ function proc_mem_rss:long (pid:long)
     mm = @task(task)->mm
     if (mm != 0)
         return (_stp_get_mm_counter(mm, _MM_FILEPAGES())
-                + _stp_get_mm_counter(mm, _MM_ANONPAGES()))
+                + _stp_get_mm_counter(mm, _MM_ANONPAGES())
+%(systemtap_v > "4.2" %?
+	      + _stp_get_mm_counter(mm, _MM_SHMEMPAGES())
+%)
+                )
   }
   return 0
 }

commit 590b4c8017598756c0a020fab2132145faeeb1c5

Bug: proc_mem_rss() broken on older kernels lacking MM_SHMEMPAGES.
This is a regression introduced in commit de253786000.

buildrun.cxx
@@ -440,6 +440,9 @@ compile_pass (systemtap_session& s)
   output_autoconf(s, o, cs, "autoconf-cpu-clock.c", "STAPCONF_CPU_CLOCK", NULL);
   output_autoconf(s, o, cs, "autoconf-local-clock.c", "STAPCONF_LOCAL_CLOCK", NULL);
   
+  // used by tapset/linux/proc_mem.stp
+  output_autoconf(s, o, cs, "autoconf-mm-shmempages.c", "STAPCONF_MM_SHMEMPAGES", NULL);
+
   // used by runtime/uprobe-inode.c
   output_either_exportconf(s, o2, "uprobe_register", "register_uprobe",
 			   "STAPCONF_UPROBE_REGISTER_EXPORTED");

runtime/linux/autoconf-mm-shmempages.c
@@ -1,0 +1,5 @@
+#include <linux/sched.h>
+
+void foo (void) {
+    (void) MM_SHMEMPAGES;
+}

tapset/linux/proc_mem.stp
@@ -73,9 +73,14 @@ enum {
 
 %(systemtap_v > "4.2" %?
 @__private30 function _MM_SHMEMPAGES:long()
-{
-	return @const("MM_SHMEMPAGES")
-}
+%{ /* pure */
+    STAP_RETVALUE =
+#ifdef STAPCONF_MM_SHMEMPAGES
+	MM_SHMEMPAGES;
+#else
+	0;
+#endif
+%}
 %)
 
 @__private30 function _stp_get_mm_counter:long(mm_ptr:long, member:long)