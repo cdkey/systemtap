cdkey/systemtap

commit 0e1d5b7eb39785cbafe0c0fb1c4e5e3d21576aab

@var() diagnostics: generate more dwarf detail & context
For TLS-related variables like @var("errno") in glibc, some
useful diagnostic details were just dropped on the floor as
semantic_error objects were caught but not printed.  New code
tries to arrange for these to be chained instead of lost.  In
exchange, more errors would propagate to session.print_error
as chained to the target_symbol, so now the code also has to
duplicate-eliminate the chained errors.

The net result is that at verbosity 0 or 1, we appear to get only a
few specific and valuable incremental messages, which point to the
problematic dwarf.  It would be nice to simplify this stuff, perhaps
by storing context (script-source level data, dwarf pointers, dwarf
contents, etc.) as RAII style global stack of "current details".

session.cxx
@@ -2369,11 +2369,11 @@ systemtap_session::print_error (const semantic_error& se)
     }
 
   // duplicate elimination
-  if (verbose > 0 || seen_errors[se.errsrc_chain()] < 1)
+  if (verbose > 0 || seen_errors[se.errsrc_chain()]++ < 1)
     {
-      seen_errors[se.errsrc_chain()]++;
       for (const semantic_error *e = &se; e != NULL; e = e->get_chain())
-        cerr << build_error_msg(*e);
+        if (verbose > 1 || seen_errors[e->errsrc]++ < 1) // dupe-eliminate chained errors too
+          cerr << build_error_msg(*e);
     }
   else suppressed_errors++;
 }

dwflpp.cxx
@@ -2788,9 +2788,10 @@ dwflpp::find_variable_and_frame_base (vector<Dwarf_Die>& scopes,
                                            vardie);
   if (declaring_scope < 0)
     {
+      // XXX: instead: add suggested locals and let a caller throw a single error
       set<string> locals;
       get_locals(scopes, locals);
-      string sugs = levenshtein_suggest(local, locals); // probably not that many, so no limit
+      string sugs = levenshtein_suggest(local, locals, 5);
       if (pc)
         throw SEMANTIC_ERROR (_F("unable to find local '%s', [man error::dwarf] dieoffset %s in %s, near pc %s %s %s %s (%s)",
                                  local.c_str(),
@@ -3245,7 +3246,18 @@ dwflpp::translate_location(location_context *ctx,
   ctx->pc = pc;
   ctx->dw = this;
 
-  return ctx->translate_location (expr, len, input);
+  try
+    {
+      return ctx->translate_location (expr, len, input);
+    }
+  catch (const semantic_error& er)
+    {
+      // copy lower level loc2stap exception; add a DIE# to it so user
+      // has a chance to find the problematic raw debuginfo
+      semantic_error err(er);
+      err.details.push_back(die_location_as_string(die));
+      throw err;
+    }
 }
 
 

tapsets.cxx
@@ -5066,8 +5066,10 @@ dwarf_atvar_query::atvar_query_cu (Dwarf_Die * cudie, dwarf_atvar_query *q)
     }
   catch (const semantic_error& er)
     {
-      // Here we suppress the error because we often just have too many
-      // when scanning all the CUs.
+      if (q->sess.verbose > 3)
+        clog << "chaining to " << q->e.tok << endl
+             << q->sess.build_error_msg(er) << endl;
+      q->e.chain (er);
       return DWARF_CB_OK;
     }
 

commit 405f69a11a6b9c4cf2585bd0bb8f9e8d60f48437

session.cxx: fix print error dupe-elimination for chained errors
Commit 0e1d5b7eb397 introduced an issue where error messages would be
duplicated, like so:
Before:
--------------------8<--------------------
semantic error: type mismatch (long): identifier 'a' at test.stp:8:5
        source:     a = 32;
                    ^

semantic error: type was first inferred here (string): identifier 'a' at :4:5
        source:     a = "stringcheese";
                    ^

Pass 2: analysis failed.  [man error::pass2]
-------------------->8--------------------

After:
--------------------8<--------------------
semantic error: type mismatch (long): identifier 'a' at test.stp:8:5
        source:     a = 32;
                    ^

semantic error: type mismatch (long): identifier 'a' at :8:5
        source:     a = 32;
                    ^

semantic error: type was first inferred here (string): identifier 'a' at :4:5
        source:     a = "stringcheese";
                    ^

Pass 2: analysis failed.  [man error::pass2]
-------------------->8--------------------

The first message would be duplicated because the wrong seen_errors is
checked inside the loop, after that first message would be printed
outside the loop. This fixes the issue by using the same error counter
throughout.

session.cxx
@@ -2391,9 +2391,9 @@ systemtap_session::print_error (const semantic_error& se)
       seen_errors[se.errsrc_chain()]++;
       cerr << build_error_msg(se);
       for (const semantic_error *e = &se; e != NULL; e = e->get_chain())
-        if (verbose > 1 || seen_errors[e->errsrc] < 1) // dupe-eliminate chained errors too
+        if (verbose > 1 || seen_errors[e->errsrc_chain()] < 1) // dupe-eliminate chained errors too
           {
-            seen_errors[e->errsrc]++;
+            seen_errors[e->errsrc_chain()]++;
             cerr << build_error_msg(*e);
           }
     }