cdkey/systemtap

commit 619f6940d8ca36bc2a0845c2b34646f26d30dfb7

PR26697: fix NULL pointer deref in get_utrace_lock()
task_utrace_struct() can return NULL via __task_utrace_struct(). This fixes
the following crash:
BUG: unable to handle kernel NULL pointer dereference at           (null)
 #9 [ffff8843e56ffd20] get_utrace_lock at ffffffffc08258c6 [stap_X_40544]

The reason why it can return NULL is because engine->ops is protected by
utrace->lock, but we don't have the utrace pointer, and the purpose of
get_utrace_lock() is to get the utrace pointer. Therefore, there's no way
to ensure engine->ops remains unchanged inside get_utrace_lock(), so
get_utrace_lock()'s checks on engine->ops can be incorrect/stale, which
leads to the NULL pointer dereference.

Signed-off-by: Yichun Zhang (agentzh) <yichun@openresty.com>

runtime/stp_utrace.c
@@ -1090,6 +1090,9 @@ static struct utrace *get_utrace_lock(struct task_struct *target,
 	}
 
 	utrace = task_utrace_struct(target);
+	if (unlikely(!utrace))
+		return ERR_PTR(-ESRCH);
+
 	spin_lock(&utrace->lock);
 	if (unlikely(utrace->reap) || unlikely(!engine->ops) ||
 	    unlikely(engine->ops == &utrace_detached_ops)) {

commit d5c4be8aad8b8b3a7890ebc049f08b7353bf68fc

stp_utrace: add missing rcu_read_unlock() in get_utrace_lock() error path
This bug was introduced in 619f6940d ("PR26697: fix NULL pointer deref
in get_utrace_lock()), which was my first stap commit, and subsequently
where this project started going downhill as a result.

runtime/stp_utrace.c
@@ -1041,8 +1041,10 @@ static struct utrace *get_utrace_lock(struct task_struct *target,
 
 	bucket = get_utrace_bucket(target);
 	utrace = task_utrace_struct(bucket, target);
-	if (unlikely(!utrace))
+	if (unlikely(!utrace)) {
+		rcu_read_unlock();
 		return ERR_PTR(-ESRCH);
+	}
 
 	spin_lock(&utrace->lock);
 	if (unlikely(utrace->reap) || unlikely(!engine->ops) ||