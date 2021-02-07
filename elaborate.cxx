cdkey/systemtap

commit 88db3a1971d45dc2133db8a4784f0a9b8a509635

PR25841: python2+3 exploitation
Reworking aspects of the python tapset & translator logic to make it
work again, after an extended period away from the plantation.  This
involved several bits:

Let all the the python[23].stp tapset functions use @cast()s that
extract debuginfo from the invoking HelperSDT.so file, not from some
always-obsolete list of libpython.so file names.  This was enabled by
the per-probe-point cloning features pulled in recent commits.  This
requires the _HelperSDT.c file to include the DWARF data normally
pulled from libpython, which is easy ... except for a few libpython3.7
internal data structures related to dicts.  Those few decls are
copy-and-pasted (eww).  python 3.6 support will need tweaks.

The python_{print,sprint} backtrace() functions are now provided
directly by the python[23].stp tapsets, as overloaded functions.  They
determine the eligibility by testing $$$$provider against "HelperSDT2"
or "HelperSDT3" to identify the python version in effect in the
invoking python extension .so.  The $$$$FOOBAR syntax is an
undocumented (sssh!) escape hatch for tapset functions invoked from
python*.module* probes which, to bypass one round of python $context
var expansion, and to land in the second round of sys/sdt.h $context
var expansion.

This relies on dead_control_remover being run eagerly & early during
symbol resolution, just as const folding, to eliminate unreachable
code.  (Letting python3 HelperSDT.so functions try to resolve python2
data types would fail during pass 2.)

The generated procfs("_stp_python*_probes") {} probes now have a dummy
exit() body, so that probe-with-empty-body elision added in 2019 keeps
them.

elaborate.cxx
@@ -5062,6 +5062,7 @@ static int semantic_pass_const_fold (systemtap_session& s, bool& relaxed_p)
 }
 
 
+
 struct dead_control_remover: public traversing_visitor
 {
   systemtap_session& session;
@@ -5081,6 +5082,7 @@ struct dead_control_remover: public traversing_visitor
 };
 
 
+
 void dead_control_remover::visit_block (block* b)
 {
   vector<statement*>& vs = b->statements;
@@ -5091,8 +5093,9 @@ void dead_control_remover::visit_block (block* b)
       vs[i]->visit (this);
       if (vs[i] == control)
         {
-          session.print_warning(_("statement will never be reached"),
-                                vs[i + 1]->tok);
+          if (session.verbose > 3) // suppress in PR25841 context
+            session.print_warning(_("statement will never be reached"),
+                                  vs[i + 1]->tok);
           vs.erase(vs.begin() + i + 1, vs.end());
           relaxed_p = false;
           break;
@@ -5175,8 +5178,9 @@ void dead_overload_remover::visit_functioncall(functioncall *e)
       for (unsigned fd = reachable; fd < e->referents.size(); fd++)
         {
           functiondecl* r = e->referents[fd];
-          s.print_warning(_("instance of overloaded function will "
-                            "never be reached"), r->tok);
+          if (s.verbose > 3) // suppress in PR25841 context
+            s.print_warning(_("instance of overloaded function will "
+                              "never be reached"), r->tok);
         }
       e->referents.erase(e->referents.begin()+reachable, e->referents.end());
       relaxed_p = false;
@@ -5691,6 +5695,12 @@ semantic_pass_optimize1 (systemtap_session& s)
       else if (iterations > 0)
         s.suppress_warnings = true;
 
+      if (s.unoptimized) // need these even in -u mode
+        {
+          semantic_pass_opt1 (s, relaxed_p);
+          semantic_pass_opt5 (s, relaxed_p);
+          semantic_pass_dead_control (s, relaxed_p);
+        }
       if (!s.unoptimized)
         {
           semantic_pass_opt1 (s, relaxed_p);

elaborate.h
@@ -508,8 +508,6 @@ struct const_folder: public update_visitor
   void visit_target_symbol (target_symbol* e);
 };
 
-
-
 // Run the given code filter visitors against the given body.
 // Repeat until they all report having relaxed.
 template <class T>
@@ -535,7 +533,7 @@ void update_visitor_loop (systemtap_session& sess, std::vector<update_visitor*>&
 template <class X, class Y>
 void var_expand_const_fold_loop(systemtap_session& sess, X& body, Y& v)
 {
-  bool relaxed_p; /* ignored */
+  bool relaxed_p = false;
   const_folder cf (sess, relaxed_p);
   std::vector<update_visitor*> k;
   k.push_back (& v);

python/HelperSDT/_HelperSDT.c
@@ -1,5 +1,5 @@
 // systemtap python SDT marker C module
-// Copyright (C) 2016 Red Hat Inc.
+// Copyright (C) 2016-2020 Red Hat Inc.
 //
 // This file is part of systemtap, and is free software.  You can
 // redistribute it and/or modify it under the terms of the GNU General
@@ -9,6 +9,68 @@
 #include <Python.h>
 #include <sys/sdt.h>
 #include <stdlib.h>
+
+
+// PR25841: ensure that the libHelperSDT.so file contains debuginfo
+// for the tapset helper functions, so they don't have to look into libpython*
+#include <frameobject.h>
+PyFrameObject _dummy_frame;
+#include <object.h>
+PyVarObject _dummy_var;
+#include <dictobject.h>
+PyDictObject _dummy_dict;
+#include <listobject.h>
+PyListObject _dummy_list;
+#include <tupleobject.h>
+PyTupleObject _dummy_tuple;
+#include <unicodeobject.h>
+PyUnicodeObject _dummy_unicode;
+
+#if PY_MAJOR_VERSION < 3
+
+#include <stringobject.h>
+PyStringObject _dummy_string;
+#include <classobject.h>
+PyClassObject _dummy_class;
+PyDictEntry _dummy_dictentry;
+PyInstanceObject _dummy_instance;
+
+#else
+
+PyASCIIObject _dummy_ascii;
+PyCompactUnicodeObject _dummy_compactunicode;
+// PyStringObject _dummy_string;
+#include <bytesobject.h>
+PyBytesObject _dummy_bytes;
+#include <longobject.h>
+PyLongObject _dummy_long;
+
+/* This is internal to libpython. */
+#if PY_MINOR_VERSION == 7  /* python 3.7 */
+typedef Py_ssize_t (*dict_lookup_func)(PyDictObject *mp, PyObject *key, Py_hash_t hash, PyObject **value_addr);
+typedef struct {
+  Py_hash_t me_hash;
+  PyObject *me_key;
+  PyObject *me_value;
+} PyDictKeyEntry;
+PyDictKeyEntry _dummy_dictkeyentry;
+struct _dictkeysobject {
+  Py_ssize_t dk_refcnt;
+  Py_ssize_t dk_size;
+  dict_lookup_func dk_lookup;
+  Py_ssize_t dk_usable;
+  Py_ssize_t dk_nentries;
+  char dk_indices[];
+};
+#endif
+
+#endif
+
+#if PY_MAJOR_VERSION < 3
+#define PROVIDER HelperSDT2
+#else
+#define PROVIDER HelperSDT3
+#endif
 
 static PyObject *
 trace_callback(PyObject *self, PyObject *args)
@@ -30,28 +92,28 @@ trace_callback(PyObject *self, PyObject *args)
     case PyTrace_CALL:
 #pragma push_macro("PyTrace_CALL")
 #undef PyTrace_CALL
-	STAP_PROBE4(HelperSDT, PyTrace_CALL, module_name, key,
+	STAP_PROBE4(PROVIDER, PyTrace_CALL, module_name, key,
 		    frame_obj, arg_obj);
 #pragma pop_macro("PyTrace_CALL")
 	break;
     case PyTrace_EXCEPTION:
 #pragma push_macro("PyTrace_EXCEPTION")
 #undef PyTrace_EXCEPTION
-	STAP_PROBE4(HelperSDT, PyTrace_EXCEPTION, module_name, key,
+	STAP_PROBE4(PROVIDER, PyTrace_EXCEPTION, module_name, key,
 		    frame_obj, arg_obj);
 #pragma pop_macro("PyTrace_EXCEPTION")
 	break;
     case PyTrace_LINE:
 #pragma push_macro("PyTrace_LINE")
 #undef PyTrace_LINE
-	STAP_PROBE4(HelperSDT, PyTrace_LINE, module_name, key,
+	STAP_PROBE4(PROVIDER, PyTrace_LINE, module_name, key,
 		    frame_obj, arg_obj);
 #pragma pop_macro("PyTrace_LINE")
 	break;
     case PyTrace_RETURN:
 #pragma push_macro("PyTrace_RETURN")
 #undef PyTrace_RETURN
-	STAP_PROBE4(HelperSDT, PyTrace_RETURN, module_name, key,
+	STAP_PROBE4(PROVIDER, PyTrace_RETURN, module_name, key,
 		    frame_obj, arg_obj);
 #pragma pop_macro("PyTrace_RETURN")
 	break;
@@ -155,7 +217,7 @@ init_HelperSDT(void)
 	// reference which stap can't parse.
 	void *fptr = &PyObject_GenericGetAttr;
 	asm ("nop" : "=r"(fptr) : "r"(fptr));
-	STAP_PROBE2(HelperSDT, Init, stap_module, fptr);
+	STAP_PROBE2(PROVIDER, Init, stap_module, fptr);
     }
     return module;
 #endif

tapset-python.cxx
@@ -79,18 +79,6 @@ public:
 };
 
 
-struct python_functioncall_expanding_visitor: public update_visitor
-{
-  python_functioncall_expanding_visitor (systemtap_session& s, int pv)
-    : sess(s), python_version(pv) {}
-
-  systemtap_session& sess;
-  int python_version;
-
-  void visit_functioncall (functioncall* e);
-};
-
-
 struct python_var_expanding_visitor: public var_expanding_visitor
 {
   python_var_expanding_visitor(systemtap_session& s, int pv)
@@ -233,44 +221,17 @@ python_derived_probe_group::enroll (python_derived_probe* p)
 
 
 void
-python_functioncall_expanding_visitor::visit_functioncall (functioncall* e)
-{
-  // If it isn't one of the functions we're interested in, we're done.
-  if (e->function != "python_print_backtrace"
-      && e->function != "python_sprint_backtrace")
-    {
-      provide (e);
-      return;
-    }
-
-  // Construct a new function call that replaces the generic python
-  // backtrace function call with a python version specific call with
-  // the right argument.
-  target_symbol *tsym = new target_symbol;
-  tsym->tok = e->tok;
-  tsym->name = "$arg3";
-  tsym->synthetic = true;
-
-  functioncall *fcall = new functioncall;
-  fcall->tok = e->tok;
-  if (python_version == 2)
-    fcall->function = (e->function == "python_print_backtrace"
-		       ? "python2_print_backtrace"
-		       : "python2_sprint_backtrace");
-  else
-    fcall->function = (e->function == "python_print_backtrace"
-		       ? "python3_print_backtrace"
-		       : "python3_sprint_backtrace");
-  fcall->type = e->type;
-  fcall->type_details = e->type_details;
-  fcall->args.push_back (tsym);
-  provide (fcall);
-}
-
-
-void
 python_var_expanding_visitor::visit_target_symbol (target_symbol* e)
 {
+  // PR25841: provide an escape to the underlying dwarf variables
+  // Preserve $$$XYZ as $XYZ into the underlying dwarf probe
+  if (e->name.size() > 3 && e->name.substr(0,3) == "$$$")
+    {
+      e->name = e->name.substr(2);
+      provide(e);
+      return;
+    }
+  
     // Convert '$$parms', '$$locals', and '$$vars' references to a
     // function call.
     if (e->name == "$$parms" || e->name == "$$locals" || e->name == "$$vars")
@@ -396,6 +357,12 @@ python_var_expanding_visitor::visit_target_symbol (target_symbol* e)
 	  }
 	repr_fcall->args.push_back(last_fcall);
 
+        // NB: we might someday need to process the generated tapset
+        // functioncalls recursively, in case they have require
+        // $$$FOOBAR -> $FOOBAR sdt.h context-var passthrough.
+
+        // require (repr_fcall);
+        
 	provide (repr_fcall);
 	return;
       }
@@ -535,10 +502,10 @@ python_builder::build(systemtap_session & sess, probe * base,
 	  stringstream code;
 	  const token* tok = base->body->tok;
 
-	  // Notice this synthetic probe has no body. That's OK,
+	  // Notice this synthetic probe has a dummy body. That's OK,
 	  // since we'll point it at our internal buffer.
-	  code << "probe procfs(\"_stp_python2_probes\").read {" << endl;
-	  code << "}" << endl;
+          // NB: it cannot be empty, because then we optimize it away.
+	  code << "probe procfs(\"_stp_python2_probes\").read { exit() } " << endl;
 	  probe *base_probe = parse_synthetic_probe (sess, code, tok);
 	  if (!base_probe)
 	    throw SEMANTIC_ERROR (_("can't create python2 procfs probe"),
@@ -564,10 +531,10 @@ python_builder::build(systemtap_session & sess, probe * base,
 	  stringstream code;
 	  const token* tok = base->body->tok;
 
-	  // Notice this synthetic probe has no body. That's OK,
+	  // Notice this synthetic probe has a dummy body. That's OK,
 	  // since we'll point it at our internal buffer.
-	  code << "probe procfs(\"_stp_python3_probes\").read {" << endl;
-	  code << "}" << endl;
+          // NB: it cannot be empty, because then we optimize it away.
+	  code << "probe procfs(\"_stp_python3_probes\").read { exit() } " << endl;
 	  probe *base_probe = parse_synthetic_probe (sess, code, tok);
 	  if (!base_probe)
 	    throw SEMANTIC_ERROR (_("can't create python3 procfs probe"),
@@ -590,7 +557,7 @@ python_builder::build(systemtap_session & sess, probe * base,
 	  stringstream code2;
 	  code2 << "probe process(\"" << PYTHON3_BASENAME
 		<< "\").library(\"" << PY3EXECDIR
-		<< "/HelperSDT/_HelperSDT.*.so\").provider(\"HelperSDT\")"
+		<< "/HelperSDT/_HelperSDT.*.so\").provider(\"HelperSDT*\")"
 		<< ".mark(\"Init\") {"
 		<< endl;
 	  code2 << "  if (user_string($arg1) != module_name()) { next }" << endl;
@@ -617,7 +584,7 @@ python_builder::build(systemtap_session & sess, probe * base,
 	  // a wildcard to find it.
 	   << (python_version == 2 ? "/HelperSDT/_HelperSDT.so\")"
 	       : "/HelperSDT/_HelperSDT.*.so\")")
-	   << ".provider(\"HelperSDT\").mark(\""
+	   << ".provider(\"HelperSDT*\").mark(\""
 	   << (has_return ? "PyTrace_RETURN"
 	       : (has_call ? "PyTrace_CALL" : "PyTrace_LINE"))
 	   << "\") {" << endl;
@@ -653,9 +620,9 @@ python_builder::build(systemtap_session & sess, probe * base,
       // The 'key' values aren't unique system wide (and just start at
       // 0), but the combination of module name and key is unique
       // system-wide.
-      code << "  if ($arg2 != "
+      code << "  if ($$$arg2 != "
 	   << (python_version == 2 ? python2_key++ : python3_key++)
-	   << " || user_string($arg1) != module_name()) { next }";
+	   << " || user_string($$$arg1) != module_name()) { next }";
       code << "}" << endl;
 
       probe *mark_probe = parse_synthetic_probe (sess, code, tok);
@@ -673,17 +640,23 @@ python_builder::build(systemtap_session & sess, probe * base,
 
       // Note that order *is* important here. We want to expand python
       // variable requests in the probe body first, then expand python
-      // backtrace requests in the probe body. The latter uses '$arg3'
+      // backtrace requests in the probe body. The latter uses '$$$arg3'
       // as the python frame pointer, and we don't want the python
       // variable exander to find those maker argument references.
       python_var_expanding_visitor pvev (sess, python_version);
-      var_expand_const_fold_loop (sess, base_copy->body, pvev);
       
-      python_functioncall_expanding_visitor v (sess, python_version);
-      v.replace (base_copy->body);
-
       // Splice base_copy->body in after the parsed body
       mark_probe->body = new block(mark_probe->body, base_copy->body);
+
+      if (sess.symbol_resolver)
+        sess.symbol_resolver->current_probe = mark_probe;
+      
+      // apply pvev ONCE only, to map $$$var -> $var (and later -> sdt.h)
+      // rather than $$$var -> $var -> python-tapset-call (now)
+      pvev.replace(mark_probe->body);
+      // const-fold-loop will be done as a part of the uprobe building
+      // var_expand_const_fold_loop (sess, mark_probe->body, pvev);
+
       derive_probes(sess, mark_probe, finished_results);
 
       // Create a python_derived_probe, but don't return it in

tapset/python2.stp
@@ -50,78 +50,55 @@
 
 #
 # Macros to cast to various python 2 types
+# PR25841 makes this easy
 #
 
 ###########################################
-#
-# FIXME: We've got a couple of problems with @cast() here.
-#
-# 1) The first thing in the @cast module list is 'python'. On some
-#    systems, the python 2 executable is named
-#    'python2'. Systemtap's configure script finds the right name for
-#    the python 2 executable, but that information doesn't make it
-#    here. This file may need to be generated at systemtap build time
-#    (i.e. have a 'python2.stp.in' tapset file that gets turned into
-#    'python2.stp').
-#
-# 2) The 2nd and 3rd items in the @cast module list is 64-bit and
-#    32-bit versions of 'libpython2.7.so'. That is the name of the
-#    current version of python 2 library on most systems. On older
-#    systems, that version number might be different.  If we could use
-#    wildcards in @cast module paths we could work around this problem
-#    (see PR20394). To work around this issue, we'll include 2.6
-#    versions of the shared library in the @cast module list. 
-#
-###########################################
-
-@define Py2ModuleList %(
-    "python:/usr/lib64/libpython2.7.so:/usr/lib/libpython2.7.so:/usr/lib64/libpython2.6.so:/usr/lib/libpython2.6.so"
-%)
 
 @define Py2Object(object) %(
-    @cast(@object, "PyObject", @Py2ModuleList)
+    @cast(@object, "PyObject")
 %)
 @define Py2VarObject(object) %(
-    @cast(@object, "PyVarObject", @Py2ModuleList)
+    @cast(@object, "PyVarObject")
 %)
 @define Py2IntObject(object) %(
-    @cast(@object, "PyIntObject", @Py2ModuleList)
+    @cast(@object, "PyIntObject")
 %)
 @define Py2StringObject(object) %(
-    @cast(@object, "PyStringObject", @Py2ModuleList)
+    @cast(@object, "PyStringObject")
 %)
 @define Py2UnicodeObject(object) %(
-    @cast(@object, "PyUnicodeObject", @Py2ModuleList)
+    @cast(@object, "PyUnicodeObject")
 %)
 @define Py2TypeObject(object) %(
-    @cast(@object, "PyTypeObject", @Py2ModuleList)
+    @cast(@object, "PyTypeObject")
 %)
 @define Py2TupleObject(object) %(
-    @cast(@object, "PyTupleObject", @Py2ModuleList)
+    @cast(@object, "PyTupleObject")
 %)
 @define Py2ListObject(object) %(
-    @cast(@object, "PyListObject", @Py2ModuleList)
+    @cast(@object, "PyListObject")
 %)
 @define Py2DictObject(object) %(
-    @cast(@object, "PyDictObject", @Py2ModuleList)
+    @cast(@object, "PyDictObject")
 %)
 @define Py2DictEntry(object) %(
-    @cast(@object, "PyDictEntry", @Py2ModuleList)
+    @cast(@object, "PyDictEntry")
 %)
 @define Py2FrameObject(object) %(
-    @cast(@object, "PyFrameObject", @Py2ModuleList)
+    @cast(@object, "PyFrameObject")
 %)
 @define Py2CodeObject(object) %(
-    @cast(@object, "PyCodeObject", @Py2ModuleList)
+    @cast(@object, "PyCodeObject")
 %)
 @define Py2LongObject(object) %(
-    @cast(@object, "PyLongObject", @Py2ModuleList)
+    @cast(@object, "PyLongObject")
 %)
 @define Py2ClassObject(object) %(
-    @cast(@object, "PyClassObject", @Py2ModuleList)
+    @cast(@object, "PyClassObject")
 %)
 @define Py2InstanceObject(object) %(
-    @cast(@object, "PyInstanceObject", @Py2ModuleList)
+    @cast(@object, "PyInstanceObject")
 %)
 
 #
@@ -908,18 +885,16 @@ function Py2Object_GetAttr:long(object:long, attr:string)
 #
 
 /*
- * python2_print_backtrace - Print python backtrace
+ * python_print_backtrace - Print python backtrace
  *
  * Description: This function is equivalent to
- * print(python2_sprint_backtrace(frame)), except that deeper stack
+ * print(python_sprint_backtrace(frame)), except that deeper stack
  * tracing may be supported. 
- *
- * Note that users should call 'python_print_backtrace()', and the
- * translator will call the appropriate python 2 or python 3 function
- * with the correct argument.
  */
-function python2_print_backtrace:long(frame:long)
+function python_print_backtrace:long()
 {
+    if ($$$$provider != "HelperSDT2") next;
+    frame = $$$arg3;
     printf("Traceback (most recent call first):\n")
     while (frame != 0) {
 	lineno = Py2Frame_GetLineNumber(frame)
@@ -942,18 +917,16 @@ function python2_print_backtrace:long(frame:long)
 }
 
 /*
- * python2_sprint_backtrace - Get python backtrace
+ * python_sprint_backtrace - Get python backtrace
  * 
  * Description: This function returns a string containing a python
  * backtrace.  Output may be truncated as per maximum string length
  * (MAXSTRINGLEN).
- *
- * Note that users should call 'python_sprint_backtrace()', and the
- * translator will call the appropriate python 2 or python 3 function
- * with the correct argument.
  */
-function python2_sprint_backtrace:string(frame:long)
+function python_sprint_backtrace:string()
 {
+    if ($$$$provider != "HelperSDT2") next;  
+    frame = $$$arg3;
     retstr = "Traceback (most recent call first):\n"
     while (frame != 0) {
 	lineno = Py2Frame_GetLineNumber(frame)

tapset/python3.stp
@@ -53,88 +53,67 @@ private global Py3Object_GenericGetAttr_addr = 0
 
 #
 # Macros to cast to various python 3 types
+# PR25841 makes this easy
 #
-
-###########################################
-#
-# FIXME: We've got a couple of problems with @cast() here.
-#
-# 1) The first thing in the @cast module list is 'python3'. On some
-#    systems, the python3 executable is just named
-#    'python'. Systemtap's configure script finds the right name for
-#    the python 3 executable, but that information doesn't make it
-#    here. This file may need to be generated at systemtap build time
-#    (i.e. have a 'python3.stp.in' tapset file that gets turned into
-#    'python3.stp').
-#
-# 2) Because @cast module paths doesn't support wildcards (see
-#    PR20394), we have to list 64-bit and 32-bit versions of the
-#    python 3.4, 3.5, and 3.6 versions of the shared library.
-#
-###########################################
-
-@define Py3ModuleList %(
-    "python3:/usr/lib64/libpython3.6.so:/usr/lib64/libpython3.6m.so:/usr/lib64/libpython3.5m.so:/usr/lib64/libpython3.4m.so:/usr/lib/libpython3.6.so:/usr/lib/libpython3.6m.so:/usr/lib/libpython3.5m.so:/usr/lib/libpython3.4m.so"
-%)
 
 @define Py3Object(object) %(
-    @cast(@object, "PyObject", @Py3ModuleList)
+    @cast(@object, "PyObject")
 %)
 @define Py3VarObject(object) %(
-    @cast(@object, "PyVarObject", @Py3ModuleList)
+    @cast(@object, "PyVarObject")
 %)
 @define Py3LongObject(object) %(
-    @cast(@object, "PyLongObject", @Py3ModuleList)
+    @cast(@object, "PyLongObject")
 %)
 @define Py3BytesObject(object) %(
-    @cast(@object, "PyBytesObject", @Py3ModuleList)
+    @cast(@object, "PyBytesObject")
 %)
 @define Py3UnicodeObject(object) %(
-    @cast(@object, "PyUnicodeObject", @Py3ModuleList)
+    @cast(@object, "PyUnicodeObject")
 %)
 @define Py3TypeObject(object) %(
-    @cast(@object, "PyTypeObject", @Py3ModuleList)
+    @cast(@object, "PyTypeObject")
 %)
 @define Py3TupleObject(object) %(
-    @cast(@object, "PyTupleObject", @Py3ModuleList)
+    @cast(@object, "PyTupleObject")
 %)
 @define Py3ListObject(object) %(
-    @cast(@object, "PyListObject", @Py3ModuleList)
+    @cast(@object, "PyListObject")
 %)
 @define Py3SetObject(object) %(
-    @cast(@object, "PySetObject", @Py3ModuleList)
+    @cast(@object, "PySetObject")
 %)
 @define Py3DictObject(object) %(
-    @cast(@object, "PyDictObject", @Py3ModuleList)
+    @cast(@object, "PyDictObject")
 %)
 @define Py3DictEntry(object) %(
-    @cast(@object, "PyDictEntry", @Py3ModuleList)
+    @cast(@object, "PyDictEntry")
 %)
 @define Py3DictKeysObject(object) %(
-    @cast(@object, "PyDictKeysObject", @Py3ModuleList)
+    @cast(@object, "PyDictKeysObject")
 %)
 @define Py3DictKeyEntry(object) %(
-    @cast(@object, "PyDictKeyEntry", @Py3ModuleList)
+    @cast(@object, "PyDictKeyEntry")
 %)
 @define Py3FrameObject(object) %(
-    @cast(@object, "PyFrameObject", @Py3ModuleList)
+    @cast(@object, "PyFrameObject")
 %)
 @define Py3CodeObject(object) %(
-    @cast(@object, "PyCodeObject", @Py3ModuleList)
+    @cast(@object, "PyCodeObject")
 %)
 
 @define Py3ASCIIObject(object) %(
-    @cast(@object, "PyASCIIObject", @Py3ModuleList)
+    @cast(@object, "PyASCIIObject")
 %)
 @define Py3ASCIIObject_Sizeof %(
-    @cast_module_sizeof(@Py3ModuleList, "PyASCIIObject")
+    @cast_sizeof("PyASCIIObject")
 %)
 
 @define Py3CompactUnicodeObject(object) %(
-    @cast(@object, "PyCompactUnicodeObject", @Py3ModuleList)
+    @cast(@object, "PyCompactUnicodeObject")
 %)
 @define Py3CompactUnicodeObject_Sizeof %(
-    @cast_module_sizeof(@Py3ModuleList, "PyCompactUnicodeObject")
+    @cast_sizeof("PyCompactUnicodeObject")
 %)
 
 #
@@ -599,7 +578,9 @@ private function Py3List_Repr:string(object:long)
 
 @define DK_ENTRIES(dk) %(
      (@choose_defined(@Py3DictKeysObject(@dk)->dk_entries,
-      (&@Py3DictKeyEntry(&@Py3DictKeysObject(@dk)->dk_indices->as_1[@DK_SIZE(@dk) * @DK_IXSIZE(@dk)]))))
+                      (&@Py3DictKeyEntry(&@Py3DictKeysObject(@dk)->dk_indices[@DK_SIZE(@dk) * @DK_IXSIZE(@dk)]))))
+     // XXX: python 3.6?
+     //      (&@Py3DictKeyEntry(&@Py3DictKeysObject(@dk)->dk_indices->as_1[@DK_SIZE(@dk) * @DK_IXSIZE(@dk)]))))
 %)
 
 private function Py3Dict_GetItem:long(op:long, key:string)
@@ -886,18 +867,16 @@ function Py3Object_GetAttr:long(object:long, attr:string)
 #
 
 /*
- * python3_print_backtrace - Print python backtrace
+ * python_print_backtrace - Print python backtrace
  *
  * Description: This function is equivalent to
- * print(python3_sprint_backtrace(frame)), except that deeper stack
+ * print(python_sprint_backtrace(frame)), except that deeper stack
  * tracing may be supported. 
- *
- * Note that users should call 'python_print_backtrace()', and the
- * translator will call the appropriate python 2 or python 3 function
- * with the correct argument.
  */
-function python3_print_backtrace:long(frame:long)
+function python_print_backtrace:long()
 {
+    if ($$$$provider != "HelperSDT3") next;
+    frame = $$$arg3;    
     printf("Traceback (most recent call first):\n")
     while (frame != 0) {
 	lineno = Py3Frame_GetLineNumber(frame)
@@ -920,18 +899,16 @@ function python3_print_backtrace:long(frame:long)
 }
 
 /*
- * python3_sprint_backtrace - Get python backtrace
+ * python_sprint_backtrace - Get python backtrace
  * 
  * Description: This function returns a string containing a python
  * backtrace.  Output may be truncated as per maximum string length
  * (MAXSTRINGLEN).
- *
- * Note that users should call 'python_sprint_backtrace()', and the
- * translator will call the appropriate python 2 or python 3 function
- * with the correct argument.
  */
-function python3_sprint_backtrace:string(frame:long)
+function python_sprint_backtrace:string()
 {
+    if ($$$$provider != "HelperSDT3") next;
+    frame = $$$arg3;
     retstr = "Traceback (most recent call first):\n"
     while (frame != 0) {
 	lineno = Py3Frame_GetLineNumber(frame)

testsuite/systemtap.python/python3.exp
@@ -10,7 +10,7 @@ if {! [file exists "$PYTHON3"]} then { untested $test; return }
 # To make sure the custom python module is in python's path, we'll
 # modify PYTHONPATH.
 set env(PYTHONPATH) $env(PY3EXECDIR)
-
+verbose -log "PYTHONPATH=$env(PYTHONPATH)"
 #
 # Test python backtraces.
 #

commit b432da7cb2bc4f28a4d3dbce429c2af40af50800

PR27361: make --dump-functions work again
Part of the work in commit 88db3a197 (version 4.3)
caused a regression with --dump-functions, wherein
optimizers would eliminate uncalled functions, even
in dump mode.  We now suppress this again and extend
the test case to look for a few more functions.

elaborate.cxx
@@ -5726,7 +5726,7 @@ semantic_pass_optimize1 (systemtap_session& s)
       else if (iterations > 0)
         s.suppress_warnings = true;
 
-      if (s.unoptimized) // need these even in -u mode
+      if (s.unoptimized && ! s.dump_mode) // need these even in -u mode
         {
           semantic_pass_opt1 (s, relaxed_p);
           semantic_pass_opt5 (s, relaxed_p);

testsuite/systemtap.base/dump_functions.exp
@@ -41,8 +41,16 @@ if {[catch {exec stap -v --dump-functions 2>/dev/null} outputv]} {
 set output  [split $output  "\n"]
 set outputv [split $outputv "\n"]
 
+verbose -log "output: $output"
+
 set subtest "user_string in output"
 has_function $output 1 "user_string:string *"
+
+set subtest "ubacktrace in output"
+has_function $output 1 "ubacktrace:string *"
+
+set subtest "callers in output"
+has_function $output 1 "callers:string *"
 
 set subtest "_caller_match not in output"
 has_function $output 0 "_caller_match:long *"