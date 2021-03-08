cd7b3e061924c3f6623cd5ad6879a91078ce4354
Serhei Makarov <smakarov@redhat.com>
2021-03-08 15:55:35 -0500

    PR27030 bpf/uconversions.stp: other sizes user_*_error()
    * tapset/bpf/uconversions.stp (__bpf_probe_read_user_error): Don't shift by 8-size.
    (__signext): New function.
    (__bpf_probe_read_user_error_signed): New function.
    (user_*_error): New functions, all from tapset/uconversions.stp.
    * tapset/uconversions.stp (user_*_error): Disable these definitions on bpf backend.
    * testsuite/systemtap.bpf/bpf-target.c: Extend test to probe different size values.
    * testsuite/systemtap.bpf/bpf_target_tests/user_long.stp: Ditto.

--- testsuite/systemtap.bpf/bpf-target.c
+++ testsuite/systemtap.bpf/bpf-target.c
@@ -1,15 +1,28 @@
+// XXX compile with -g without -O
+
 //#include <stdio.h>
 
+// for user_long.stp, user_atvar_error.stp
 long a = 3;
+
+// for user_long.stp
+int i = -3; // 0x
+short j = -2; // 0xfffe
+char k = -1; // 0xff
 
 void foo(long *b) {
   (*b)++;
+}
+
+void bar(int *b, short *c, char *d) {
+  (void)b; (void)c; (void)d;
 }
 
 int main(void) {
   //printf("initial a: %d\n", a);
   a++;
   foo(&a);
+  bar(&i,&j,&k);
   //printf("final a: %d\n", a);
   return 0;
 }
